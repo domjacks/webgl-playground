6d09c6394da2445f65f62455065e0c53ca4eb072
Dom Jackson <dom.jackson@bbc.co.uk>
2025-11-30 18:17:10 +0000

    Add Black Rectangle and destroy after 2 seconds

--- lightning-renderer-example/src/main.js
+++ lightning-renderer-example/src/main.js
@@ -1,12 +1,22 @@
 import { setupLightningRenderer } from "./setupLightningRenderer";
 
 async function main() {
-
   const appElement = document.getElementById("app");
-
   const { renderer, canvasWrapper } = await setupLightningRenderer();
-
   appElement.appendChild(canvasWrapper);
+
+    const myNode = renderer.createNode({
+      parent: renderer.root,
+      x: 2,
+      y: 2,
+      w: 100,
+      h: 100,
+      color: 0x000000ff,
+    });
+
+    setTimeout(() => {
+      myNode.destroy()
+    }, 2000);
 }
 
 await main();
